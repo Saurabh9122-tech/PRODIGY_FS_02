24dbc3500463dc9742e9670e6ba9008252f15fcd
Saurabh9122-tech <ss061959@gmail.com>
2025-06-17 12:16:12 +0530

    connect the database

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,8 +1,9 @@
 const mongoose = require('mongoose');
+require('dotenv').config(); // ✅ Load .env
 
 const connectDB = async () => {
   try {
-    await mongoose.connect('mongodb://localhost:27017/prodigy_employee_db', {
+    await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
